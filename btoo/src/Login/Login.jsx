e6318fb05ab7e627b8da0d1be92ce35188999758
KJS <digyrh456789@gmail.com>
2022-09-13 22:00:11 +0900

    W

--- btoo/src/Login/Login.jsx
+++ btoo/src/Login/Login.jsx
@@ -4,11 +4,15 @@ export default function Login() {
         <div className="loginpg">
             <p className="logm">로그인</p>
             <div className="loginbox">
+                <p id="loging">ID</p>
             </div>
             <div className="passwordbox">
-
+                <p id="passwording">PASSWORD</p>
             </div>
-            <div className="letlog"></div>
+            <div className="letlog">
+                <p id="letgo">Let's Go!</p>
+            </div>
+            <p id="singup">회원가입</p>
         </div>
     );
 }
---
+++ btoo/src/Login/Signup.jsx
@@ -0,0 +1,7 @@
+export default function Singup() {
+    return(
+        <div>
+            
+        </div>
+    );
+}
