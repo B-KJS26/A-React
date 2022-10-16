248226cd3a9875f12e87eca725ac5063e8f091e5
KJS <80664816+B-KJS26@users.noreply.github.com>
2022-10-16 23:25:54 +0900

    Chore | Add some tag

--- one/src/Hello.jsx
+++ one/src/Hello.jsx
@@ -1,7 +1,10 @@
 function Hello({ color, name, isSpecial }) {
     return <div style={{ color }}>
         {isSpecial && <b>Wow</b>}
-        {name}</div>
+        {name}
+        <h1>색, 이름, 확인</h1>
+        <p>Hello, Today is Sunday</p>
+        </div>
 }
 Hello.defaultProps = {
     name: '이름없음'
