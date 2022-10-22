7f43a6672ab303383d5e82c37eee382b182521c3
KJS <digyrh456789@gmail.com>
2022-10-22 23:53:27 +0900

    Chore | make chat page

--- btoo/src/App.jsx
+++ btoo/src/App.jsx
@@ -1,6 +1,7 @@
 import './components/Header/Header.css';
 import Login from './components/Login/Login';
 import Signup from './components/Login/Signup';
+import Chat from './components/Chat/Chat';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Login/>}></Route>
           <Route path='/signup' element={<Signup/>}></Route>
+          <Route path='chat' element={<Chat />}></Route>
         </Routes>
       </Router>
     </div>
---
+++ btoo/src/components/Chat/Chat.jsx
@@ -0,0 +1,8 @@
+export default function Chat() {
+    return(
+        <div>
+            <h1>Chat</h1>
+            <input type="text" placeholder="텍스트를 입력해주세요."></input>
+        </div>
+    )
+}
