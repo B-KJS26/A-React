8f7967a32e443199a8f3002315ea5170b068870a
KJS <digyrh456789@gmail.com>
2022-10-25 22:17:20 +0900

    FIX | 화면 안나오던 버그 수정 자잘한 것 추가

--- one/src/components/Header/Header.jsx
+++ one/src/components/Header/Header.jsx
@@ -1,8 +1,13 @@
+import { Navigate } from "react-router-dom"
+import './Header.css';
 export default function Header() {
+    function Logins() {
+        //페이지 이동 코드 추가할 예정
+    }
     return(
         <div>
-            <h2>민준민준챗</h2>
-            <text>Log in / Sign up</text>
+            <h2 className="title">민준민준챗</h2>
+            <text onClick={Logins} className="loginso">Log in / Sign up</text>
         </div>
     )
 }
--- one/src/components/Main.jsx
+++ one/src/components/Main.jsx
@@ -1,7 +1,11 @@
+import Header from './Header/Header'
+
 export default function Main(){
     return(
         <div>
+            <Header/>
             <h1>안녕하세용, 민준민준챗입니다.</h1>
+            <p>친구들과 혹은 모르는 사람들과 채팅을 나눠보시던가-</p>
         </div>
     )
 }
