37241d2089a49fe8f4f6c12b2ab07c89c9b97f6c
KJS <80664816+B-KJS26@users.noreply.github.com>
2022-10-29 23:59:14 +0900

    Chore | name, chatlog

--- btoo/src/components/Chat/Chat.jsx
+++ btoo/src/components/Chat/Chat.jsx
@@ -1,8 +1,12 @@
 export default function Chat() {
+    let name;
+    let chatlog;
     return(
         <div>
             <h1>Chat</h1>
             <input type="text" placeholder="텍스트를 입력해주세요."></input>
+            <h1>{name}</h1>
+            <p>{chatlog}</p>
         </div>
     )
 }
