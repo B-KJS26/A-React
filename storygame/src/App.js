98becabcafe1ee9341c3c05c43925aba8cd472da
KJS <digyrh456789@gmail.com>
2022-12-19 19:37:26 +0900

    zㅣ존용제나이트의 모험 웹 게임

---
+++ storygame/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import './pages/gamepage/Lobbypage';
+import './pages/Main';
+import react from 'react';
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
---
+++ storygame/src/pages/Main.js
@@ -0,0 +1,6 @@
+import react from 'react';
+export default function Main() {
+    <div>
+        <p>zㅣ존용제나이트의 모험</p>
+    </div>
+}
