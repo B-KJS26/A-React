057af86b5cbfff3fa839a3b57e2892f58f65924e
KJS <digyrh456789@gmail.com>
2022-12-27 22:18:15 +0900

    FEAT :: FOOTER

--- spotify_clone/src/App.js
+++ spotify_clone/src/App.js
@@ -11,6 +11,7 @@ function App() {
         <Routes>
           <Route path='/' element={<MainPage/>}></Route>
         </Routes>
+      <Footer/>
       </BrowserRouter>
     </div>
   );
---
+++ spotify_clone/src/pages/Footer/Footer.js
@@ -0,0 +1,38 @@
+
+export default function Footer() {
+    return(
+        <div>
+            <div>
+                <img src='images/logo.png'></img>
+                <div>
+                    <p>회사</p>
+                    <p>상세정보</p>
+                    <p>채용 정보</p>
+                    <p>For the Record</p>
+                </div>
+                <div>
+                    <p>커뮤니티</p>
+                    <p>아티스트</p>
+                    <p>개발자</p>
+                    <p>투자자</p>
+                    <p>공급업체</p>
+                </div>
+                <div>
+                    <p>유용한 링크</p>
+                    <p>지원</p>
+                    <p>앱 플레이어</p>
+                    <p>For the Record</p>
+                </div>
+                <div>
+                    <img src="images/insta.png"></img>
+                </div>
+                <div>
+                    <img src="images/twitter.png"></img>
+                </div>
+                <div>
+                    <img src="images/facebook.png"></img>
+                </div>
+            </div>
+        </div>
+    )
+}
