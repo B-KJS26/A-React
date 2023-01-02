abf4b1a9767bbfdbb65713599b1bfd66765754eb
KJS <digyrh456789@gmail.com>
2023-01-02 11:29:15 +0900

    UPDATE :: 스포티파이 API 연결, 로그인 시 유저 프로필, 닉네임 불러오기 성공

--- spotify_clone/src/pages/Footer/Footer.js
+++ spotify_clone/src/pages/Footer/Footer.js
@@ -22,37 +22,46 @@ export default function Footer() {
             <img src='images/logo.png' className="spolog" onClick={footerlog}></img>
             <div className='company'>
                 <p className='comtext'>회사</p>
-                <p>상세정보</p>
-                <p>채용 정보</p>
-                <p>For the Record</p>
+                <div className='declick'>
+                    <p>상세정보</p>
+                    <p>채용 정보</p>
+                    <p>For the Record</p>
+                </div>
             </div>
             <div className='community'>
                 <p className='communitytext'>커뮤니티</p>
-                <p>아티스트</p>
-                <p>개발자</p>
-                <p>투자자</p>
-                <p>공급업체</p>
+                <div className='declick'>
+                    <p>아티스트</p>
+                    <p>개발자</p>
+                    <p>투자자</p>
+                    <p>공급업체</p>
+                </div>
             </div>
             <div className='link'>
                 <p className='linktext'>유용한 링크</p>
-                <p>지원</p>
-                <p>앱 플레이어</p>
-                <p>For the Record</p>
+                <div className='declick'>
+                    <p>지원</p>
+                    <p>앱 플레이어</p>
+                </div>
             </div>
-            <div className='instabox'>
-                <img src="images/insta.png" className='instaimg' onClick={goinsta}></img>
-            </div>
-            <div className='twitterbox'>
-                <img src="images/twitter.png" className='twimg' onClick={gotwit}></img>
-            </div>
-            <div className='facebox'>
-                <img src="images/facebook.png" className='faceimg' onClick={goface}></img>
+            <div className='wowz'>
+                <div className='instabox'>
+                    <img src="images/insta.png" className='instaimg' onClick={goinsta}></img>
+                </div>
+                <div className='twitterbox'>
+                    <img src="images/twitter.png" className='twimg' onClick={gotwit}></img>
+                </div>
+                <div className='facebox'>
+                    <img src="images/facebook.png" className='faceimg' onClick={goface}></img>
+                </div>
             </div>
             <div className='terms'>
                 <p>
-                    <span>법률 정보</span>
-                    <span className='private'>개인정보 보호 센터</span>
-                    <span className='private2'>개인정보 처리방침</span>
+                    <span className='declick'>
+                        <span>법률 정보</span>
+                        <span className='private'>개인정보 보호 센터</span>
+                        <span className='private2'>개인정보 처리방침</span>
+                    </span>
                     <span className='copyright'>© 2022 Spotify AB</span>
                 </p>
             </div>
--- spotify_clone/src/pages/Header/Header.js
+++ spotify_clone/src/pages/Header/Header.js
@@ -1,9 +1,52 @@
 import '../../styles/Header/Header.scss'
 import React from 'react';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom'
 export default function Header() {
     let nav = useNavigate();
-    return(
+    const [token, setToken] = useState("")
+    const [searchKey, setSearchKey] = useState("")
+    const [artists, setArtists] = useState([])
+    const [profile, setProfile] = useState("");
+    const [username, setUsername] = useState("");
+    const CLIENT_ID = "4fc90538d496458f9d61549dd038375e"
+    const REDIRECT_URI = "http://localhost:3000"
+    const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
+    const RESPONSE_TYPE = "token"
+    function goLogins() {
+        window.location.href = `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`;
+    }
+    function goSign() {
+        window.location.href = 'https://www.spotify.com/kr-ko/signup';
+    }
+    useEffect(() => {
+        const hash = window.location.hash
+        let token = window.localStorage.getItem("token")
+        if (!token && hash) {
+            token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
+            window.location.hash = ""
+            window.localStorage.setItem("token", token)
+            const res = axios.get("https://api.spotify.com/v1/me", {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                },
+                params: {
+                    
+                }
+            })
+            res.then(response => setUsername(response.data.display_name));
+            res.then(response => setProfile(response.data.images[0].url));
+        }
+        console.log(username);
+        console.log(profile);
+        setToken(token)
+    }, [])
+    function Logout() {
+        setToken("")
+        window.localStorage.removeItem("token")
+    }
+    return (
         <div className='mainheader'>
             <div className='spotifyheader'>
                 <img src='images/logo.png' className='spotifylogo'></img>
@@ -11,9 +54,15 @@ export default function Header() {
                     <text>프리미엄</text>
                     <text>지원</text>
                     <text>다운로드 하기</text>
-                    <text>ㅣ</text>
-                    <text>가입하기</text>
-                    <text>로그인하기</text>
+                    {!token ?
+                        <text onClick={goSign}>회원가입하기</text>
+                        : <span className='users'>
+                        <img src={profile} className='userimg'></img>
+                        <text>{username}님</text>
+                        </span>}
+                    {!token ?
+                        <text onClick={goLogins}>로그인하기</text>
+                        : <text onClick={Logout}>로그아웃</text>}
                 </div>
             </div>
         </div>
