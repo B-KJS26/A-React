7aa2175b7d8c8ca9b93b8c0e325d1960030b4feb
KJS <digyrh456789@gmail.com>
2023-01-11 23:53:04 +0900

    UPDATE :: 결제 도움말 페이지 업데이트

--- spotify_clone/src/pages/Help/Pay/Pay.js
+++ spotify_clone/src/pages/Help/Pay/Pay.js
@@ -1,8 +1,22 @@
 import react from 'react';
+import '../../../styles/Help/Pay/Pay.scss'
+import { IoIosArrowForward } from 'react-icons/io';
 export default function Pay() {
-    return(
+    return (
         <div>
-
+            <div className='paybg'>
+                <div className='gohome'>
+                    <span>홈</span><IoIosArrowForward className='arrowing' />
+                </div>
+                <h1 className='rnao'>결제 도움말</h1>
+                <div>
+                    <span className='rufwp'>
+                        <span className='rhksfl'>결제 정보 관리</span>
+                    </span>
+                    <span className='tneks'>결제 수단</span>
+                    <span className='ehdnaakf'>충전 관련 도움말</span>
+                </div>
+            </div>
         </div>
     )
 }
