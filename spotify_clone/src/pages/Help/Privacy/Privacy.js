54a6dbf12cb54a3ad68258a6f621b318cbe436f7
KJS <digyrh456789@gmail.com>
2023-01-16 23:49:31 +0900

    UPDATE :: 안전 및 개인정보 보호 페이지 요소 추가

--- spotify_clone/src/pages/Help/Privacy/Privacy.js
+++ spotify_clone/src/pages/Help/Privacy/Privacy.js
@@ -1,7 +1,42 @@
+import react from 'react';
+import '../../../styles/Help/Privacy/Privacy.scss'
+import { SlArrowRight } from 'react-icons/sl';
+import { useNavigate } from 'react-router-dom';
 export default function Privacy() {
-    return(
+    const nav = useNavigate();
+    function goback() {
+        nav('/help');
+    }
+    return (
         <div>
-            
+            <div className='privacybg'>
+                <div className='goback' onClick={goback}>
+                    <span>홈</span><SlArrowRight className='arrowing' />
+                </div>
+                <h1 className='priv'>안전 및 개인정보 보호</h1>
+                <div>
+                    <span className='options'>
+                        <span className='op'>데이터 및 개인정보 관련 정보</span>
+                    </span>
+                    <span className='options2'>
+                        <span className='op2'>플랫폼 규정</span>
+                    </span>
+                    <span className='options3'>
+                        <span className='op3'>콘텐츠에 대한 조치</span>
+                    </span>
+                </div>
+                <div className='privating'>
+                    <div className='pving'>
+                        <span>데이터 권리 및 개인정보 설정</span><SlArrowRight size='25' className='arrows' />
+                    </div>
+                    <div className='pving2'>
+                        <span>내 데이터 이해하기</span><SlArrowRight size='25' className='arrows1' />
+                    </div>
+                    <div className='pving3'>
+                        <span>GDPR 제15조 정보</span><SlArrowRight size='25' className='arrows2' />
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
