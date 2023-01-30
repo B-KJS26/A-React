1d0f2555508e873fb38474fd238170009d6184ae
KJS <digyrh456789@gmail.com>
2023-01-30 23:19:21 +0900

    UPDATE :: 지원 페이지 이동 기능

--- spotify_clone/src/pages/Help/Pay/Pay.js
+++ spotify_clone/src/pages/Help/Pay/Pay.js
@@ -7,6 +7,24 @@ export default function Pay() {
     function goback() {
         nav('/help');
     }
+    function onething() {
+        window.location.href = 'https://support.spotify.com/kr-ko/article/payments-duo-plan/';
+    }
+    function secondthing() {
+        window.location.href = 'https://support.spotify.com/kr-ko/article/update-payment-details/';
+    }
+    function thirdthing() {
+        window.location.href = 'https://support.spotify.com/kr-ko/article/view-receipts/';
+    }
+    function fourththing() {
+        window.location.href = 'https://support.spotify.com/kr-ko/article/billing-date/';
+    }
+    function fifththing() {
+        window.location.href = 'https://support.spotify.com/kr-ko/article/cancel-premium/';
+    }
+    function sixththing() {
+        window.location.href = 'https://support.spotify.com/kr-ko/article/why-cant-i-get-premium-in-the-app/';
+    }
     return (
         <div>
             <div className='paybg'>
@@ -26,22 +44,22 @@ export default function Pay() {
                     </span>
                 </div>
                 <div className='payinghelping'>
-                    <div className='duoing'>
+                    <div className='duoing' onClick={onething}>
                         <span>듀오 요금제에 대한 결제</span><SlArrowRight size='25' className='arrows' />
                     </div>
-                    <div className='duoing2'>
+                    <div className='duoing2' onClick={secondthing}>
                         <span>결제 세부 정보 업데이트</span><SlArrowRight size='25' className='arrows' />
                     </div>
-                    <div className='duoing3'>
+                    <div className='duoing3' onClick={thirdthing}>
                         <span>영수증 보기</span><SlArrowRight size='25' className='arrows3' />
                     </div>
-                    <div className='duoing4'>
+                    <div className='duoing4' onClick={fourththing}>
                         <span>결제일</span><SlArrowRight size='25' className='arrows4' />
                     </div>
-                    <div className='duoing5'>
+                    <div className='duoing5' onClick={fifththing}>
                         <span>Premium 요금제 해지하기</span><SlArrowRight size='25' className='arrows5' />
                     </div>
-                    <div className='duoing6'>
+                    <div className='duoing6' onClick={sixththing}>
                         <span>왜 앱 내에서 Premium에 가입할 수 없나요?</span><SlArrowRight size='25' className='arrows6' />
                     </div>
                 </div>
