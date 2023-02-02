0433b9dcd277aa4260ad1748ec5f7e4d38230c95
KJS <digyrh456789@gmail.com>
2023-02-02 23:58:18 +0900

    ADD :: 챕터 페이지 추가

--- storygame/src/pages/gamepage/Selectch.js
+++ storygame/src/pages/gamepage/Selectch.js
@@ -1,12 +1,22 @@
 import '../../styles/gamepage/Selectch.scss';
 import react from 'react';
 export default function Selectch() {
+    const [number, setNumber] = useState(1);
+    function setone() {
+        setNumber(1);
+    }
+    function settwo() {
+        setNumber(2);
+    }
+    function setthree() {
+        setNumber(3);
+    }
     return(
         <div className='selectchapters'>
             <h1>챕터를 선택해주세요</h1>
-            <p>- Chapter 1 -</p>
-            <p>- Chapter 2 -</p>
-            <p>- Chapter 3 -</p>
+            <p onClick={setone}>- Chapter 1 -</p>
+            <p onClick={settwo}>- Chapter 2 -</p>
+            <p onClick={setthree}>- Chapter 3 -</p>
         </div>
     )
 }
