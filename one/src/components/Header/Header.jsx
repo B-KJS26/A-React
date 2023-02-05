5c8006e89fedb14cdb7492f9e71455aa37f1175e
KJS <digyrh456789@gmail.com>
2023-02-05 23:56:30 +0900

    UPDATE :: 컴포넌트 구조 변경, 헤더 수정

--- one/src/components/Header/Header.jsx
+++ one/src/components/Header/Header.jsx
@@ -1,6 +1,8 @@
 import { Navigate } from "react-router-dom"
 import { useNavigate } from "react-router-dom/dist";
-import './Header.css';
+import Yo from '../../img/discord.svg';
+import Yo2 from '../../img/discord2.svg';
+import './Header.scss';
 export default function Header() {
     const nav = useNavigate();
     function Logins() {
@@ -8,8 +10,15 @@ export default function Header() {
     }
     return(
         <div className="headers">
-            <h2 className="title">Discord</h2>
-            <text onClick={Logins} className="loginso">Log in</text>
+            <img src={Yo} className="img1"></img>
+            <img src={Yo2} className="img2"></img>
+            <span className="title">Discord</span>
+            <span>Download</span>
+            <span>Nitro</span>
+            <span>Discover</span>
+            <span>Safety</span>
+            <span>Support</span>
+            <button><text onClick={Logins} className="loginso">Log in</text></button>
         </div>
     )
 }
--- one/src/components/Main.jsx
+++ one/src/components/Main.jsx
@@ -1,11 +1,9 @@
 import Header from './Header/Header'
-
+import './Main.scss'
 export default function Main(){
     return(
-        <div>
+        <div className='mainpages'>
             <Header/>
-            <h1>안녕하세용, 민준민준챗입니다.</h1>
-            <p>친구들과 혹은 모르는 사람들과 채팅을 나눠보시던가-</p>
         </div>
     )
 }
