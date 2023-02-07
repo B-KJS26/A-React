530d7eba66349b1b0e9b74efd66ee160aba17509
KJS <digyrh456789@gmail.com>
2023-02-07 23:59:02 +0900

    UPDATE :: Mainserve1 Page 완성

--- one/src/components/Main.jsx
+++ one/src/components/Main.jsx
@@ -1,9 +1,11 @@
 import Header from './Header/Header'
 import './Main.scss'
+import Serveone from './Mainserve/Serve1'
 export default function Main(){
     return(
         <div className='mainpages'>
             <Header/>
+            <Serveone/>
         </div>
     )
 }
--- one/src/components/Mainserve/Serve1.jsx
+++ one/src/components/Mainserve/Serve1.jsx
@@ -1,9 +1,10 @@
 import one from '../../img/first.svg'
-
+import './Serve1.scss'
 export default function Serveone() {
     return(
-        <div>
-            <img src={one}></img>
+        <div className='serveone'>
+            <img src={one} className='imging'></img>
+            <h1>나만을 위한 초대<br/>전용 공간을 만들<br/>어보세요</h1>
         </div>
     )
 }
