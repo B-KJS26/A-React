c16c953d129217d378310e70a73f9734101bbfa5
KJS <digyrh456789@gmail.com>
2023-02-11 22:12:28 +0900

    UPDATE :: 다운로드 페이지 상세 정보 등 추가

--- one/src/components/Download/Download.jsx
+++ one/src/components/Download/Download.jsx
@@ -1,11 +1,16 @@
 import react from 'react';
 import Header from '../Header/Header';
-
-
+import './Download.scss';
+import  Downloadings from '../../img/downloading.svg';
 export default function Download() {
     return(
-        <div>
+        <div className='download'>
             <Header/>
+            <div className='downloading'>
+                <h1>어느 장치에서든<br/>DISCORD 설치하기</h1>
+                <p>모험이 기다리고 있어요. PC 앱으로 친구들과 어울리고 모바일에<br/>서도 대화를 이어가세요.</p>
+                <img src={Downloadings} className='downicon'></img>
+            </div>
         </div>
     )
 }
