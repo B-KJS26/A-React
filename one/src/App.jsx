369b7494c7f661acc1a25bd3d5345f34921180c9
KJS <digyrh456789@gmail.com>
2023-02-20 22:04:53 +0900

    ADD :: Discover Page

--- one/src/App.jsx
+++ one/src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Download from './components/Download/Download';
 import Footer from './components/Footer/Footer';
 import Nitro from './components/Nitro/Nitro';
+import Discover from './components/Discover/Discover';
 export default function App() {
   return (
     <Router>
@@ -11,6 +12,7 @@ export default function App() {
         <Route path='/' element={<Main/>}></Route>
         <Route path='/download' element={<Download/>}></Route>
         <Route path='/nitro' element={<Nitro/>}></Route>
+        <Route path='/discover' element={<Discover/>}></Route>
       </Routes>
       <Footer/>
     </Router>
---
+++ one/src/components/Discover/Discover.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+export default function Discover() {
+    return(
+        <div>
+            
+        </div>
+    );
+}
